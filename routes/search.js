bc0df0ba121898b77615d5a1e2406a6bde01131e
hqzzzz <hqzzzz@126.com>
2025-11-14 16:44:19 +0800

    限制查询数量

--- routes/search.js
+++ routes/search.js
@@ -20,9 +20,9 @@ async function searchRoutes (fastify, opts) {
       console.log('keyword search' + JSON.stringify(req.query));
       const { data } = await api.get(`/search?query=${encodeURIComponent(req.query.keyword)}`);
       const items = data.items || [];
-      const jobs = items.filter(it => it['magnet-flg'] === 1);
-      const movieJobs = jobs.filter(it => it.media_type === 'movie');
-      const tvJobs = jobs.filter(it => it.media_type === 'tv');
+      const jobs = items.filter(it => it['magnet-flg'] === 1).slice(0, 3);
+      const movieJobs = jobs.filter(it => it.media_type === 'movie').slice(0, 2);
+      const tvJobs = jobs.filter(it => it.media_type === 'tv').slice(0, 2);
 
       const [movieRows, tvRows] = await Promise.all([
         Promise.all(movieJobs.map(fetchMovieMagnets)),
