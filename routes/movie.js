9f2f1e4cb34f4d5a666df6fb7ec719de467695bf
hqzzzz <hqzzzz@126.com>
2025-11-17 08:50:47 +0800

    先拿元数据

--- routes/movie.js
+++ routes/movie.js
@@ -3,6 +3,14 @@ const { api, parseSize } = require('./common');
 /** 传入单条 movie 对象，返回 [{title,download,size...}, ...] */
 async function fetchMovieMagnets(item) {
   const { tmdbid, title, poster, vote_average, release_date } = item;
+
+
+   // 先拿元数据
+  const { data: detail } = await api.get(`/movie/${tmdbid}`);
+  if (!detail || detail['magnet-flg'] !== 1) {
+    return [];          // 没有磁铁资源，直接空数组
+  }
+
   const { data } = await api.get(`/movie/${tmdbid}/magnet`);
   return (data.magnet || []).map(m => ({
     title: m.name || title,
